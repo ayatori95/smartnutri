250f329c648471c4b42259adb43d49e8c7e434bf
Ayatori <lorenacristinaac@gmail.com>
2023-06-08 00:13:19 -0300

    Adicação das rotas receitas e receitasId para acessar os controllers de receitas

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bodyParse = require('body-parser')
 const userController = require('../controllers/userController');
+const receitaController = require('../controllers/receitasController');
 
 router.use(bodyParse.urlencoded({extended:true}));
 
@@ -13,5 +14,11 @@ router.get('/usuario/:id', userController.exibirUsuario);
 
 // Rota POST para salvar um usuário
 router.post('/usuarios', userController.salvarUsuario);
+
+// Rota get para exibir uma lista de receitas
+router.get('/receitas', receitaController.receitas);
+
+// Rota get para exibir receitas específicas do usuário
+router.get('/receita/:id', receitaController.receitasId);
 
 module.exports = router;
